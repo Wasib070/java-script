563d4efac08f6e8fc45ca75b44615cba428ab50c
wasib <wasib070@gmail.com>
2025-10-25 23:24:36 +0530

    adding first code

---
+++ basics.js
@@ -0,0 +1,8 @@
+const accountId=144523
+let accountEmail='wasib@gmail.com' //let has block scope
+var accountPassword=12345 // var has function scope
+
+// const accountId=23 not allowed to change
+console.log(accountId)
+
+console.log(accountEmail,accountId,accountPassword)
