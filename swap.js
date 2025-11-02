503d530dc255715251f248861906a97eb33bb77e
wasib <wasib070@gmail.com>
2025-11-02 16:39:52 +0530

    add code

---
+++ swap.js
@@ -0,0 +1,11 @@
+let a="5";
+let b="8";
+console.log(`before swapping a= ${a} and b= ${b}`);
+console.log("type of a and b",typeof(a),typeof(b));
+
+let temp=a;
+a=b;
+b=temp;
+console.log(`after swapping a= ${a} and b= ${b}`);
+
+
