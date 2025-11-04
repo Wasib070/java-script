15497cd8186ce379b9a4b2476b125f18ed497da4
wasib <wasib070@gmail.com>
2025-11-04 20:47:34 +0530

    modified

--- stringg1.js
+++ stringg1.js
@@ -1,2 +1,7 @@
-let name= prompt
-console.log(name.toUpperCase());
+// let name= prompt("enter the name:\n");
+// console.log(name.toUpperCase());
+let n='wasib'
+let n1=n.slice(1,)
+console.log(n1)
+let tweet="abcdefghijklmnopq"
+console.log(tweet.toUpperCase())
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+// let name= prompt("enter the name:\n");
+// console.log(name.toUpperCase());
+let n='wasib'
+console.log(name.slice(1,))
