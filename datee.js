bf9d51650bd3092ff0f067114ed3bb87175db0a9
wasib <wasib070@gmail.com>
2025-11-15 18:20:56 +0530

    ..

---
+++ datee.js
@@ -0,0 +1,25 @@
+let mydate= new Date();
+console.log(mydate);
+console.log(">>>>>>>.toString()",mydate.toString());
+console.log(">>>>>>>.toDateString",mydate.toDateString);
+console.log(">>>>>>>.toLocalString()",mydate.toLocaleString());
+console.log(typeof mydate);
+let myCreatedDate= new Date(2025,9,9);
+console.log(myCreatedDate.toLocaleString());
+let myTimeStamp=Date.now();
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+let newDate=new Date();
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+
+
+
+
+
+
+
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,8 +1,3 @@
-const score=100;
-const balance = new Number(400);
-console.log(typeof score,score);
-
-console.log(typeof balance, balance);
-console.log(score.toFixed(1));
-let num=123.8966;
-console.log(num.toPrecision(3));
+let mydate= new Date();
+console.log(mydate);
+console.log(mydate.toString());
