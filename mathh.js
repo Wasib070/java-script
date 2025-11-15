eb73a55e30a14e075eff815ff5d7e144013f7607
wasib <wasib070@gmail.com>
2025-11-15 18:06:56 +0530

    ..

---
+++ mathh.js
@@ -0,0 +1,18 @@
+console.log('Math.abs(-4) =',Math.abs(-4));
+console.log('Math.round(12.6) =',Math.round(12.6));
+console.log('Math.ceil(12.99) =',Math.ceil(12.99));
+console.log("math.floor(12.99) =",Math.floor(12.99));
+console.log("Math.Max(4,3,6,8)=",Math.max(4,3,6,8));
+console.log("Math.Min(4,3,6,8)=",Math.min(4,3,6,8));
+console.log("Math.random()=",Math.random());
+console.log("(Math.random()*10)+1=",(Math.random()*10)+1);
+console.log("Math.floor((Math.random()*10)+1)=",Math.floor((Math.random()*10)+1));
+console.log(" Random Number between 10 and 20:");
+let min =10;
+let max=20;
+console.log("Math.floor(Math.random()*(max-min +1)+min)=",Math.floor(Math.random()*(max-min +1))+min);
+
+
+
+
+
