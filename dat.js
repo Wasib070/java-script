1a61fe9341e7005c144687a34e357ff8b5d67190
wasib <wasib070@gmail.com>
2025-11-19 12:43:42 +0530

    ,

---
+++ dat.js
@@ -0,0 +1,18 @@
+let mydate= new Date();
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleString());
+console.log(typeof mydate);
+let myCreatedDate=new Date(2023,8,20);
+console.log(myCreatedDate.toLocaleString());
+let mytimestamp=Date.now();
+console.log(mytimestamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+const newdate=new Date();
+console.log(newdate);
+console.log(newdate.getMonth()+1);
+console.log(newdate.getDay()+1);
+
+
+
+
