41fc414b3afc9f49fa33e19e5e50c048a2c6e7b1
wasib <wasib070@gmail.com>
2025-11-19 12:50:15 +0530

    .

---
+++ arr.js
@@ -0,0 +1,17 @@
+const myar=[0,1,2,3,4,5,6,'abc',true]
+console.log(myar[0]);
+myar.push(999)
+console.log(myar)
+myar.pop()
+console.log(myar);
+myar.shift()
+console.log(myar);
+myar.unshift(200)
+console.log(myar);
+myar.includes(200);
+myar.indexOf(200);
+
+const varr=myar.join()
+console.log(myar);
+
+
