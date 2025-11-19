206526164c293aee577bcb1eec591b786f18c67e
wasib <wasib070@gmail.com>
2025-11-19 14:07:40 +0530

    .

---
+++ obj.js
@@ -0,0 +1,18 @@
+const user={
+  name : 'wasib',
+  age : 20,
+  location:'kmr',
+  email : "@abcc",
+  IssloggedIn: false
+}
+//acess values
+console.log(user);
+console.log(user.age);
+console.log(user['age']);
+//change values
+user.email="wasib070@gmail.com"
+console.log(user["email"]);
+
+
+
+
